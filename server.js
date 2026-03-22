edcd197163c070d1c9acf3583de5e028bc018dd7
minusInfinite <ashley@minusinfinite.net>
2026-03-22 12:57:55 +1030

    added demo seed

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.get("/stats", (req, res) =>
     res.sendFile(path.join(__dirname, "/public/stats.html"))
 )
 
+app.get("/seed", (req, res, next) => {
+    require("./seeders/seed")
+    res.send("Database Seeded")
+})
+
 mongoose
     .connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
         useNewUrlParser: true,
